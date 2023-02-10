592a4b92d279004effe73ce1a6e35b11ce3cf482
yusufyilmazf <yusufyilmaz.f@gmail.com>
2023-02-11 00:38:58 +0300

    add robot client

---
+++ client-robot/mqtt-client/mqtt-client.ino
@@ -0,0 +1,99 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <driver/ledc.h>
+
+/* change it with your ssid-password */
+const char* ssid = "Movsec 2";
+const char* password = "movsec2022!";
+/* this is the IP of PC/raspberry where you installed MQTT Server 
+on Wins use "ipconfig" 
+on Linux use "ifconfig" to get its IP address */
+const char* mqtt_server = "192.168.1.55";
+const char* topic = "moves";
+/* create an instance of PubSubClient client */
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+long lastMsg = 0;
+char msg[20];
+
+const int led1 = 26;
+const int led2 = 27;
+
+void receivedCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message received: ");
+  Serial.println(topic);
+
+  Serial.print("payload: ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  /* we got '1' -> on */
+  if ((char)payload[0] == '1') {
+    digitalWrite(led1, HIGH); 
+  } else {
+    /* we got '0' -> on */
+    digitalWrite(led1, LOW);
+  }
+
+}
+
+void mqttconnect() {
+  /* Loop until reconnected */
+  while (!client.connected()) {
+    Serial.print("MQTT connecting ...");
+    /* client ID */
+    String clientId = "ESP32Client";
+    /* connect now */
+    if (client.connect(clientId.c_str(), "guest", "guest")) {
+      Serial.println("connected");
+      /* subscribe topic with default QoS 0*/
+      client.subscribe(topic);
+    } else {
+      Serial.print("failed, status code =");
+      Serial.print(client.state());
+      Serial.println("try again in 5 seconds");
+      /* Wait 5 seconds before retrying */
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  /* set led as output to control led on-off */
+  pinMode(led1, OUTPUT);
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  /* configure the MQTT server with IPaddress and port */
+  client.setServer(mqtt_server, 1883);
+  /* this receivedCallback function will be invoked 
+  when client received subscribed topic */
+  client.setCallback(receivedCallback);
+}
+
+void loop() {
+  /* if client was disconnected then try to reconnect again */
+  if (!client.connected()) {
+    mqttconnect();
+  }
+  /* this function will listen for incomming 
+  subscribed topic-process-invoke receivedCallback */
+  client.loop();
+}
