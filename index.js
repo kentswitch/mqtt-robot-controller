5c64f17875a0aa89c476fafe1ab1ac076d40562d
Furkan ERDEM <ffrknerdm@gmail.com>
2023-02-11 00:00:42 +0300

    Change library to mqtt js Implement led on off

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
-import { connect } from 'amqplib';
+import mqtt from 'async-mqtt';
 
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+
+const sendMessages = async () => {
+
+    const options = {
+        port: 1883,
+        username: 'mqtt-user',
+        password: 'mqtt-user',
+        clientId: 'mqttjs',
+        clean: true,
+    };
+
+    const connection = await mqtt.connectAsync('mqtt://mqtt-user:mqtt-user@localhost:1883', options);
+
+    console.log("Starting");
+	try {
+		await connection.publish("moves", '1');
+		console.log("Done sending message: 1");
+        await delay(1000);
+
+        await connection.publish("moves", '0');
+		console.log("Done sending message: 0");
+        await delay(1000);
+	} catch (e){
+		// Do something about it!
+		console.log(e.stack);
+		process.exit();
+	}
+}
 
 // Async function to connect to RabbitMQ and send a message
 async function main() {
-    // Set up connection and queue
-    const queue = 'moves';
-    const connection = await connect('amqp://guest:guest@localhost:5672');
-
-    // Create a channel and queue
-    const channel = await connection.createChannel();
-    await channel.assertQueue(queue);
-
-    const message = 'Arzuhal';
-    // Send a message in given intervals
-    setInterval(() => {
-        channel.sendToQueue(queue, Buffer.from(message));
-        console.log(`Sent ${message}`);
-    }, 2500);
+    while(true){
+        await sendMessages();
+    }
 }
 
 await main();
