565e03f85efb1214a917f6dc33803fb30a661123
Furkan ERDEM <ffrknerdm@gmail.com>
2023-02-11 22:13:18 +0300

    lint

--- client-robot/mqtt-client/mqtt-client.ino
+++ client-robot/mqtt-client/mqtt-client.ino
@@ -3,27 +3,27 @@
 #include <driver/ledc.h>
 
 /* change it with your ssid-password */
-const char* ssid = "Movsec 2";
-const char* password = "movsec2022!";
-/* this is the IP of PC/raspberry where you installed MQTT Server 
-on Wins use "ipconfig" 
+const char *ssid = "Movsec 2";
+const char *password = "movsec2022!";
+/* this is the IP of PC/raspberry where you installed MQTT Server
+on Wins use "ipconfig"
 on Linux use "ifconfig" to get its IP address */
-const char* mqtt_server = "192.168.1.55";
-const char* topic = "moves";
+const char *mqtt_server = "192.168.1.55";
+const char *topic = "moves";
 /* create an instance of PubSubClient client */
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 // ########## MOTOR CONTROL ##########
 // Motor-1
-const int motorPin1 = 27; 
-const int motorPin2 = 26; 
+const int motorPin1 = 27;
+const int motorPin2 = 26;
 const int enablePin = 14; // PWM Control Pins
 
-// Motor-2 
+// Motor-2
 const int motorPin3 = 17;
 const int motorPin4 = 16;
-const int enablePin2 = 4; // PWM Control Pins 
+const int enablePin2 = 4; // PWM Control Pins
 
 // Setting PWM properties
 const int freq = 30000;
@@ -33,117 +33,138 @@ const int resolution = 8;
 
 // ########## Direction Functions ###########
 
-void rigth(){
-digitalWrite(motorPin1,HIGH);
-digitalWrite(motorPin2,LOW); 
-digitalWrite(motorPin3,LOW);
-digitalWrite(motorPin4,LOW);
+void rigth()
+{
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
 }
 
-void left(){
-digitalWrite(motorPin1,LOW);
-digitalWrite(motorPin2,LOW);
-digitalWrite(motorPin3,HIGH);
-digitalWrite(motorPin4,LOW);
-  
+void left()
+{
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
 }
 
-void forward(){
-digitalWrite(motorPin1,HIGH);
-digitalWrite(motorPin2,LOW);
-digitalWrite(motorPin3,HIGH);
-digitalWrite(motorPin4,LOW);
+void forward()
+{
+  digitalWrite(motorPin1, HIGH);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
 }
 
-void backward(){
-  digitalWrite(motorPin1,LOW);
-digitalWrite(motorPin2,HIGH);
-digitalWrite(motorPin3,LOW);
-digitalWrite(motorPin4,HIGH);
+void backward()
+{
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
 }
 
-void allLow(){
-digitalWrite(motorPin1,LOW);
-digitalWrite(motorPin2,LOW);
-digitalWrite(motorPin3,LOW);
-digitalWrite(motorPin4,LOW);
+void allLow()
+{
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
 }
 
- // ########## Duty Cycle PWM ##########
+// ########## Duty Cycle PWM ##########
 int dutyCycle1 = 200;
 int dutyCycle2 = 200;
 
 unsigned long currentTime = millis();
 // Previous time
-unsigned long previousTime = 0; 
+unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
 char moves[100];
 
-void receivedCallback(char* topic, byte* payload, unsigned int length) {
+void receivedCallback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message received: ");
   Serial.println(topic);
 
   Serial.print("payload: ");
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)payload[i]);
   }
 
   Serial.println();
-  for (int i = 0; i < 100; i++){
+  for (int i = 0; i < 100; i++)
+  {
     moves[i] = (char)payload[i];
-    if (moves[i] == 'p'){
+    if (moves[i] == 'p')
+    {
       break;
     }
   }
 
-  for(int m = 0; m < 100; m++){
+  for (int m = 0; m < 100; m++)
+  {
+    Serial.print("MOVE IS: ");
     Serial.println(moves[m]);
 
-    if (moves[m] == 'r'){
+    if (moves[m] == 'r')
+    {
       Serial.println("r");
       rigth();
     }
-    else if (moves[m] == 'l'){
+    else if (moves[m] == 'l')
+    {
       Serial.println("l");
       left();
     }
-    else if (moves[m] == 'f'){
+    else if (moves[m] == 'f')
+    {
       Serial.println("f");
       forward();
     }
-    else if (moves[m] == 'b'){
+    else if (moves[m] == 'b')
+    {
       Serial.println("b");
       backward();
     }
-    else if (moves[m] == 's'){
+    else if (moves[m] == 's')
+    {
       Serial.println("s");
       allLow();
     }
-    else if (moves[m] == 'p'){
+    else if (moves[m] == 'p')
+    {
       Serial.println("p");
       break;
     }
-    else{
+    else
+    {
       Serial.println("Error");
     }
   }
-  
 }
 
-void mqttconnect() {
+void mqttconnect()
+{
   /* Loop until reconnected */
-  while (!client.connected()) {
+  while (!client.connected())
+  {
     Serial.print("MQTT connecting ...");
     /* client ID */
     String clientId = "ESP32Client";
     /* connect now */
-    if (client.connect(clientId.c_str(), "guest", "guest")) {
+    if (client.connect(clientId.c_str(), "guest", "guest"))
+    {
       Serial.println("connected");
       /* subscribe topic with default QoS 0*/
       client.subscribe(topic);
-    } else {
+    }
+    else
+    {
       Serial.print("failed, status code =");
       Serial.print(client.state());
       Serial.println("try again in 5 seconds");
@@ -153,7 +174,8 @@ void mqttconnect() {
   }
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -162,22 +184,23 @@ void setup() {
 
   WiFi.begin(ssid, password);
 
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
   /* set led as output to control led on-off */
-   pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(enablePin, OUTPUT);
   pinMode(motorPin3, OUTPUT);
   pinMode(motorPin4, OUTPUT);
   pinMode(enablePin2, OUTPUT);
 
-  digitalWrite(motorPin1,LOW);
-  digitalWrite(motorPin2,LOW);
-  digitalWrite(motorPin3,LOW);
-  digitalWrite(motorPin4,LOW);
+  digitalWrite(motorPin1, LOW);
+  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
 
   Serial.println("");
   Serial.println("WiFi connected");
@@ -186,17 +209,19 @@ void setup() {
 
   /* configure the MQTT server with IPaddress and port */
   client.setServer(mqtt_server, 1883);
-  /* this receivedCallback function will be invoked 
+  /* this receivedCallback function will be invoked
   when client received subscribed topic */
   client.setCallback(receivedCallback);
 }
 
-void loop() {
+void loop()
+{
   /* if client was disconnected then try to reconnect again */
-  if (!client.connected()) {
+  if (!client.connected())
+  {
     mqttconnect();
   }
-  /* this function will listen for incomming 
+  /* this function will listen for incomming
   subscribed topic-process-invoke receivedCallback */
   client.loop();
 }
